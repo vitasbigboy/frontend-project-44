d76e4f8c2057eb4225f14137eb8f78ffb7aeedb2
Виталий <vitas2505@icloud.com>
2024-03-18 00:36:24 +0300

    Step 5

--- Makefile
+++ Makefile
@@ -8,3 +8,5 @@ lint:
 	npx eslint .
 lint-fix:
 	npx eslint . --fix
+brain-even:
+	node bin/brain-even.js
--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,11 @@ import readlineSync from 'readline-sync';
 
 const greeting = () => {
   console.log('Welcome to the Brain Games!');
+
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+
+  return name;
 };
 
 export default greeting;
