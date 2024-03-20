f4296e823b1629987bf45863207b626cbcbf03b4
Виталий <vitas2505@icloud.com>
2024-03-20 15:26:31 +0300

    Step 5

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/vitasbigboy/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/vitasbigboy/frontend-project-44/actions)
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/866176eaef3ef1941252/maintainability)](https://codeclimate.com/github/vitasbigboy/frontend-project-44/maintainability)
+
+[![asciicast](https://asciinema.org/a/z2Lnh1B1tXlrxbMrofnseaYuG.svg)](https://asciinema.org/a/z2Lnh1B1tXlrxbMrofnseaYuG)
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greeting from '../src/cli.js';
+import gameBody from '../src/gameBody.js';
 
-const userName = greeting();
-
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const task = () => {
   const q = Math.round(Math.random() * 100);
@@ -18,24 +15,4 @@ const task = () => {
   return [q, a];
 };
 
-let mark = 0;
-
-for (let i = 1; i <= 3; i += 1) {
-  const step = task();
-
-  console.log(`Question: ${step[0]}`);
-  const answer = readlineSync.question('Answer: ');
-
-  if (answer === step[1]) {
-    console.log('Correct!');
-    mark += 1;
-  } else {
-    console.log(`${answer} is wrong answer ;(. Correct answer was ${step[1]}.)`);
-    console.log(`Let's try again, ${userName}!`);
-    break;
-  }
-}
-
-if (mark === 3) {
-  console.log(`Congratulations, ${userName}!`);
-}
+gameBody(description, task);
